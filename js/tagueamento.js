022863901d3696ae0d559ffede00152b8f95d680
lucascr91 <lucas.ecomg@gmail.com>
2021-07-07 16:41:03 -0300

    change inline to function (download_pdf)

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -3,3 +3,7 @@
 function entre_em_contato() {
     ga('send', {'hitType': 'event', 'eventCategory': 'menu', 'eventAction': 'entre_em_contato', 'eventLabel': 'link_externo' })
 }
+
+function download_pdf() {
+    ga('send', {'hitType': 'event', 'eventCategory': 'menu', 'eventAction': 'download_pdf', 'eventLabel': 'download_pdf' });
+}
