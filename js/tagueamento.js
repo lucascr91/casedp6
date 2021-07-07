fd728ec2256695a9ec28f831977846b1db9b1a58
lucascr91 <lucas.ecomg@gmail.com>
2021-07-07 16:38:22 -0300

    change inline to function

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -1,1 +1,5 @@
 // Ver arquivos HTML
+
+function entre_em_contato() {
+    ga('send', {'hitType': 'event', 'eventCategory': 'menu', 'eventAction': 'entre_em_contato', 'eventLabel': 'link_externo' })
+}
