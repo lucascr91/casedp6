adcf1bd6c1bec94fcd07c3a888a774823ae3dbe9
lucascr91 <lucas.ecomg@gmail.com>
2021-07-07 17:43:45 -0300

    add image_click() function

--- js/tagueamento.js
+++ js/tagueamento.js
@@ -1,5 +1,3 @@
-// Ver arquivos HTML
-
 function entre_em_contato() {
     ga('send', {'hitType': 'event', 'eventCategory': 'menu', 'eventAction': 'entre_em_contato', 'eventLabel': 'link_externo' });
 }
@@ -8,7 +6,12 @@ function download_pdf() {
     ga('send', {'hitType': 'event', 'eventCategory': 'menu', 'eventAction': 'download_pdf', 'eventLabel': 'download_pdf' });
 }
 
-function image_click(img) {
-    var name = img.alt;
-    ga('send', {'hitType': 'event', 'eventCategory': 'analise', 'eventAction': 'ver_mais', 'eventLabel': name });
+// function image_click(img) {
+//     var name = img.alt;
+//     ga('send', {'hitType': 'event', 'eventCategory': 'analise', 'eventAction': 'ver_mais', 'eventLabel': name });
+// }
+
+function image_click() {
+    var src = img.src;
+    window.open(src);
 }
